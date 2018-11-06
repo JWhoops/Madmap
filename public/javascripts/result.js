cd095f21f381424d4c9e3a40f5969baa96c4d419
Jinyu Wu <q835771840@gmail.com>
2018-11-06 15:32:19 -0600

    finised item specific needs to finish show item detail

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -8,6 +8,7 @@ function initMap() {
 
   let sValue = new URLSearchParams(document.location.search.substring(1)).get("sValue");
   let resultList = $("#resultList")
+  let cMarkers = [] //current marker
  $.ajax({
     url: 'http://localhost:8080/USWISCUWMAD',
     type: 'GET',
@@ -15,7 +16,7 @@ function initMap() {
     dataType: 'jsonp',
     //callback~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     success: function(data) {
-      var buildings = data.foundBuildings//data array
+      var buildings = data.next//data array
       var center = [43.076592,-89.4124875]; 
       var hasLocation = false;
       var dist = 0;
@@ -32,23 +33,18 @@ function initMap() {
       // Try HTML5 geolocation
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
+          center = [];
           center.push(position.coords.latitude);
           center.push(position.coords.longitude);
           map.setCenter({lat:center[0],lng:center[1]});
           hasLocation = true;
-        }, function() {
-	//nothing
-	});
-      } else {
-          console.log("Browser doesn't support geolocation.");
-      }
-
-      buildings.forEach((building)=>{
-          building.utilities.forEach((utility)=>{
+          buildings.forEach((building)=>{
+            building.utilities.forEach((utility)=>{
             if(utility.type === sValue){
-	     var tDist = measureDist(center[0],center[1],building.lat,building.lng);
-	     utils.push({dist:tDist,name:building.name,description:utility.description});
-	     dist = Math.max(tDist,dist);
+              let tDist = measureDist(center[0],center[1],building.lat,building.lng);
+              utils.push({dist:Math.round(tDist),name:building.name,description:utility.description,
+                          lat:building.lat,lng:building.lng});
+              dist = Math.max(tDist,dist);
               //create mark and list~~~~~~~~~~
               creatMark(building.lat,building.lng)
               // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -59,31 +55,65 @@ function initMap() {
       //try to calculate the approporiate scale
       sortByDist(utils);
       utils.forEach((util)=>{
-      resultList.append( '<li>'+ 
-              '<img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/bascom-hall-todd-klassy.jpg" />'+
-              '<h3>'+util.name+'</h3>'+
-              '<p>'+util.description+'</p>'+
-	      '<p>'+util.dist+' meters from me</p>'+
-              '</li>');
+        resultList.append(createCard(util));
       });
-      //center: current position, utils: [] of {dist, name, description}
-      console.log(getMapSize());
-      console.log(center);
-      console.log(dist);
-      console.log(utils); 
+        }, function() {
+	   });
+      } else {
+          console.log("Browser doesn't support geolocation.");
+      }
     },
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     error: function() { alert('Failed!'); }
 });
 
+const createCard = (obj)=>{
+ let liTag = document.createElement("li"),
+     imgTag = document.createElement("img"),
+     hTag = document.createElement("h3"),
+     desTag = document.createElement("label"),
+     disTag = document.createElement("p") 
+     $(hTag).text(obj.name)
+     $(imgTag).attr("src","https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/bascom-hall-todd-klassy.jpg")
+     $(disTag).text("Distance: " + obj.dist + " meters")
+     $(desTag).text("description: " + obj.description)
+     liTag.append(imgTag)
+     liTag.append(hTag)
+     liTag.append(disTag)
+     liTag.append(desTag)
+     $(liTag).on('click',function(){
+       showSpcItem(obj)
+     })
+     return liTag
+}
+
+const showSpcItem = (obj) => {
+  resultList.empty()
+  cleanMarkers()
+  creatMark(obj.lat,obj.lng)
+  map.setCenter({lat:obj.lat,lng:obj.lng})
+  $("#pageContainer").append('<p>' + obj.name +'</p>')
+  $("#pageContainer").append('<p>'+ "Description: " + obj.description +'</p>')
+  $("#pageContainer").append('<p>'+ "Distance: " + obj.dist + " meters" +'</p>')
+  $("#pageContainer").append('<button>Get Direction</button>')
+}
+
+const cleanMarkers = () => {
+  if(cMarkers.length > 0){
+    cMarkers.forEach((cMarker)=>{
+      cMarker.setMap(null)
+    })
+  }
+}
 
 //create mark on map
 const creatMark = (lat,lng) =>{
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
               position: new google.maps.LatLng(lat, lng),
               icon: icons['info'].icon,
               map: map
             });
+  cMarkers.push(marker)
 }
 
 const measureDist = (lat1, lng1, lat2, lng2) => {
