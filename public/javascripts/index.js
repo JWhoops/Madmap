4fa383a8730346ae0eabd49a17386b6e90aa4c9b
Jinyu Wu <q835771840@gmail.com>
2018-11-17 20:41:25 -0600

    mistake

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -23,16 +23,18 @@ document.getElementById("feedbackBtn").addEventListener("click", ()=>{
 (()=>{
 	let hour = new Date().getHours(),
 		gLbl = document.getElementById("greetingLbl")
-        hour = 5
+    //morning
     if(hour>=4 && hour<12){
 		gLbl.innerHTML = "Good Morning,"
     	document.body.style.backgroundImage = "url('./public/resources/morning.jpg')";
+    //afternoon
     }else if(hour>=12 && hour<18){
     	gLbl.innerHTML = "Good Afternoon,"
     	document.body.style.backgroundImage = "url('./public/resources/afternoon.jpg')";
+    //evening
     }else{
     	gLbl.innerHTML = "Good Evening,"
     	document.body.style.backgroundImage = "url('./public/resources/evening.jpg')";   
     }
-    document.body.style.backgroundSize = "cover"
+    document.body.style.backgroundSize = "cover"//stretch the background image
 })()
