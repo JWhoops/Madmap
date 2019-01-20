0d71ac3e4ec85784ed93e046034130fe9973411b
Jinyu Wu <q835771840@gmail.com>
2019-01-20 14:25:51 -0600

    ahhhh need to apply lightbox and fix direction calculating icon

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,68 +1,53 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
 function initMap() {
-  let iconBase = "https://maps.google.com/mapfiles/kml/shapes/";
-  let icons = {
-    info: {
-      icon: iconBase + "info-i_maps.png"
+  // init direction service
+  const directionsService = new google.maps.DirectionsService();
+  const directionsDisplay = new google.maps.DirectionsRenderer();
+
+  // search value
+  const sValue = new URLSearchParams(document.location.search.substring(1)).get(
+    "sValue"
+  );
+  // result check
+  const searchResults = ["Microwave", "Printer"];
+  if (!searchResults.includes(sValue)) {
+    alert("result not found!");
+    window.location.href = "../index.html"; // redirect to main page
+  }
+
+  const resultList = $("#result-list");
+  let currentMarks = [];
+  // current marker
+  const resultCoordinates = [];
+  // record all found result coordinates
+  const bounds = new google.maps.LatLngBounds(); // record bounds
+  // remove all cureent marks on the map
+  const removeCurrentMarks = () => {
+    if (currentMarks.length > 0) {
+      currentMarks.forEach(cMarker => {
+        cMarker.setMap(null);
+      });
     }
   };
 
-  //search value
-  let sValue = new URLSearchParams(document.location.search.substring(1)).get(
-      "sValue"
-    ),
-    resultList = $("#result-list"),
-    currentMarks = [], //current marker
-    resultCoordinates = [], //record all found result coordinates
-    bounds = new google.maps.LatLngBounds(); //record bounds
-  $.ajax({
-    url: "https://angrymap.herokuapp.com/USWISCUWMAD",
-    type: "GET",
-    crossDomain: true,
-    dataType: "jsonp",
-    success: function(data) {
-      populateResults(data);
-    },
-    error: function() {
-      alert("Failed!");
-    }
-  });
-
-  const createCard = obj => {
-    let liTag = document.createElement("li"),
-      imgTag = document.createElement("img"),
-      hTag = document.createElement("h3"),
-      desTag = document.createElement("p"),
-      disTag = document.createElement("span"),
-      textContainer = document.createElement("div");
-    $(hTag).text(obj.name);
-    $(imgTag).attr("src", obj.bImage);
-    $(disTag).text(obj.dist + " miles from me");
-    if (obj.description.length > 20) $(desTag).text(obj.description);
-    else $(desTag).text(obj.description);
-    $(liTag).addClass("card");
-    $(imgTag).addClass("card-img");
-    $(hTag).addClass("card-title");
-    $(desTag).addClass("card-detail");
-    $(disTag).addClass("card-annotation");
-    $(textContainer).addClass("card-text-container");
-    liTag.append(textContainer);
-    liTag.append(imgTag);
-    textContainer.append(hTag);
-    textContainer.append(disTag);
-    textContainer.append(desTag);
-    $(liTag).on("click", function() {
-      showSpcItem(obj);
+  // create a mark on map
+  const creatMark = (lat, lng) => {
+    const marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lng),
+      map
     });
-    return liTag;
+    currentMarks.push(marker);
+    return marker;
   };
-
+  // get current geolocation
   const getGeolocation = callback => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        function(position) {
+        position => {
           callback(position.coords.latitude, position.coords.longitude);
         },
-        function(err) {
+        err => {
           switch (err.code) {
             case err.PERMISSION_DENIED:
               alert(
@@ -79,6 +64,8 @@ function initMap() {
             case err.UNKNOWN_ERROR:
               alert("An unknown error occurred.");
               break;
+            default:
+              alert("A truly unknown error");
           }
           callback(43.076592, -89.4124875);
         }
@@ -89,30 +76,166 @@ function initMap() {
       alert("Assume your position is at the center of the campus");
     }
   };
+  // current scroll position and specific mode
+  let currentScroll;
+  let specMode = false;
+  // show specific item
+  const showSpcItem = obj => {
+    specMode = true;
+    $(".nav").show();
+    currentScroll = this.scrollY;
+    window.scrollTo(0, 0); // scoll to top
+    resultList.hide(); // hide result list
+    removeCurrentMarks();
+    creatMark(obj.lat, obj.lng);
+    map.setCenter({ lat: obj.lat, lng: obj.lng });
+    map.setZoom(16);
+    // set information for specific item
+    $("#buildingName").text(`${obj.type} in ${obj.name}`);
+    $("#buildingDes").text(obj.description);
+    $("#buildingDis").text(`${obj.dist} miles`);
+    $("#utilityIMG").attr("src", obj.uImage);
+    // direction button
+    $("#directionBtn").on("click", () => {
+      getGeolocation((lat, lng) => {
+        removeCurrentMarks();
+        const request = {
+          origin: new google.maps.LatLng(lat, lng),
+          destination: new google.maps.LatLng(obj.lat, obj.lng),
+          travelMode: "WALKING"
+        };
+        directionsService.route(request, (result, status) => {
+          if (status === "OK") {
+            directionsDisplay.setDirections(result);
+          }
+        });
+      });
+    });
+    // hold to show button
+    const img = $("#utilityIMG");
+    const showBtn = $("#showIMG");
+    showBtn.text("Show Photo");
+    img.hide();
+    showBtn.on("click", () => {
+      if (showBtn.text() === "Show Photo") {
+        $("#map").hide();
+        img.show();
+        showBtn.text("Show Map");
+      } else {
+        img.hide();
+        $("#map").show();
+        showBtn.text("Show Photo");
+      }
+    });
+    $(".result-container").hide();
+    $("#detail-container").show();
+  };
+
+  const createCard = obj => {
+    const liTag = document.createElement("li");
+    const imgTag = document.createElement("img");
+    const hTag = document.createElement("h3");
+    const desTag = document.createElement("p");
+    const disTag = document.createElement("span");
+    const textContainer = document.createElement("div");
+    $(hTag).text(obj.name);
+    $(imgTag).attr("src", obj.bImage);
+    $(disTag).text(`${obj.dist} miles from me`);
+    if (obj.description.length > 20) $(desTag).text(obj.description);
+    else $(desTag).text(obj.description);
+    $(liTag).addClass("card");
+    $(imgTag).addClass("card-img");
+    $(hTag).addClass("card-title");
+    $(desTag).addClass("card-detail");
+    $(disTag).addClass("card-annotation");
+    $(textContainer).addClass("card-text-container");
+    liTag.append(textContainer);
+    liTag.append(imgTag);
+    textContainer.append(hTag);
+    textContainer.append(disTag);
+    textContainer.append(desTag);
+    $(liTag).on("click", () => {
+      showSpcItem(obj);
+    });
+    return liTag;
+  };
 
   const populateResults = data => {
-    let buildings = data.next, //data array
-      center = [43.076592, -89.4124875],
-      hasLocation = false,
-      dist = 0,
-      utils = [];
-    //how to graph map
+    const buildings = data.next;
+    // data array
+    let center = [43.076592, -89.4124875];
+    let dist = 0;
+    const utils = [];
+    // how to graph map
     map = new google.maps.Map(document.getElementById("map"), {
       mapTypeId: "roadmap",
       disableDefaultUI: true
     });
+    directionsDisplay.setMap(map);
+
+    const measureDist = (lat1, lng1, lat2, lng2) => {
+      const R = 6378.137;
+      const dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
+      const dLng = (lng2 * Math.PI) / 180 - (lng1 * Math.PI) / 180;
+      const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos((lat1 * Math.PI) / 180) *
+          Math.cos((lat2 * Math.PI) / 180) *
+          Math.sin(dLng / 2) *
+          Math.sin(dLng / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const d = R * c;
+      return d * 1000;
+    };
+
+    const sortByDist = list => {
+      // insertion sort
+      const listt = list;
+      for (let i = 1; i < list.length; i += 1) {
+        let j = i - 1;
+        while (j > -1 && list[j].dist >= list[j + 1].dist) {
+          const temp = list[j];
+          listt[j] = list[j + 1];
+          listt[j + 1] = temp;
+          j -= 1;
+        }
+      }
+    };
+    const loadMoreList = (liArr, step, btn) => {
+      const cNums = liArr.length;
+      // eslint-disable-next-line no-multi-assign
+      let pp = (cp = step); // pp=pause_point cp=current_point
+      if (cNums <= step) {
+        pp = cNums;
+        btn.hide();
+      } else {
+        btn.on("click", () => {
+          if (pp <= cNums) {
+            pp += step;
+            while (cp < pp && cp < cNums) {
+              $(createCard(liArr[cp])).insertBefore(btn);
+              cp += 1;
+            }
+          }
+          if (pp >= cNums) btn.hide(); // operation after loading all items
+        });
+      }
+      // default load
+      for (let i = 0; i < pp; i += 1) {
+        $(createCard(liArr[i])).insertBefore(btn);
+      }
+    };
 
     getGeolocation((lati, longti) => {
-      let mark; //used to store mark
+      let mark; // used to store mark
       center = [];
       center.push(lati);
       center.push(longti);
       map.setCenter({ lat: center[0], lng: center[1] });
-      hasLocation = true;
       buildings.forEach(building => {
         building.utilities.forEach(utility => {
           if (utility.type === sValue) {
-            let tDist = measureDist(
+            const tDist = measureDist(
               center[0],
               center[1],
               building.lat,
@@ -129,17 +252,17 @@ function initMap() {
               type: utility.type
             });
             dist = Math.max(tDist, dist);
-            //create mark and list~~~~~~~~~~
+            // create mark and list~~~~~~~~~~
             mark = creatMark(building.lat, building.lng);
             resultCoordinates.push({ lat: building.lat, lng: building.lng });
             // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            bounds.extend(mark.getPosition()); //add mark to bound
+            bounds.extend(mark.getPosition()); // add mark to bound
           }
         });
       });
-      map.fitBounds(bounds); //set final bounds
-      /*furthest distance:dist
-    try to calculate the approporiate scale*/
+      map.fitBounds(bounds); // set final bounds
+      /* furthest distance:dist
+      try to calculate the approporiate scale */
       sortByDist(utils);
       loadMoreList(utils, 4, $("#load-more-btn"));
       $("#back-btn").show();
@@ -149,152 +272,61 @@ function initMap() {
     });
   };
 
-  //go back button initialize
+  // go back button initialize
   $("#back-btn").on("click", () => {
-    //check display property to decide go back btn
-    let c = document.querySelector("#detail-container").style.display;
-    if (c == "none" || c == "") {
+    // check display property to decide go back btn
+    const c = document.querySelector("#detail-container").style.display;
+    if (c === "none" || c === "") {
       window.location.href = "../index.html";
     } else {
+      specMode = false; // quit spec mdoe for scroll listener
+      directionsDisplay.set("directions", null);
       $("#detail-container").hide();
       $(".result-container").show();
-      //remove all current marks
+      // check if image is on
+      if ($("#utilityIMG").is(":visible")) {
+        $("#utilityIMG").hide();
+        $("#map").show();
+      }
+      // remove all current marks
       removeCurrentMarks();
       currentMarks = [];
-      //populate the marks again
+      // populate the marks again
       resultCoordinates.forEach(coor => {
         creatMark(coor.lat, coor.lng);
       });
       $("#directionBtn").off();
       $("#showIMG").off();
-      //$("#map").css("height", "40%");
-      map.fitBounds(bounds); //use old bounds
+      map.fitBounds(bounds); // use old bounds
+      window.scrollTo(0, currentScroll); // scroll to previous position
     }
   });
 
-  //show specific ite
-  const showSpcItem = obj => {
-    resultList.hide(); //hide result list
-    removeCurrentMarks();
-    creatMark(obj.lat, obj.lng);
-    map.setCenter({ lat: obj.lat, lng: obj.lng });
-    map.setZoom(16);
-    //set information for specific item
-    $("#buildingName").text(obj.type + " in " + obj.name);
-    $("#buildingDes").text(obj.description);
-    $("#buildingDis").text(obj.dist + " miles");
-    $("#utilityIMG").attr("src", obj.uImage);
-    //direction button
-    $("#directionBtn").on("click", () => {
-      getGeolocation((lat, lng) => {
-        window.open(
-          "http://maps.apple.com/?saddr=" +
-            lat +
-            "," +
-            lng +
-            "&daddr=" +
-            obj.lat +
-            "," +
-            obj.lng +
-            "&dirflg=w"
-        );
-      });
-    });
-    //hold to show button
-    let img = $("#utilityIMG"),
-      showBtn = $("#showIMG");
-    showBtn.text("Show Photo");
-    img.css("display", "none");
-    showBtn.on("click", () => {
-      if (showBtn.text() == "Show Photo") {
-        $("#map").hide();
-        img.show();
-        showBtn.text("Show Map");
+  $.ajax({
+    url: "https://angrymap.herokuapp.com/USWISCUWMAD",
+    type: "GET",
+    crossDomain: true,
+    dataType: "jsonp",
+    success(data) {
+      populateResults(data);
+    },
+    error() {
+      alert("Failed!");
+    }
+  });
+
+  // scroll down listener
+  let scrollPos = 0; // Initial state
+  // adding scroll event
+  window.addEventListener("scroll", () => {
+    if (!specMode) {
+      // detects new state and compares it with the new one
+      if (document.body.getBoundingClientRect().top > scrollPos) {
+        $(".nav").show();
       } else {
-        img.hide();
-        $("#map").show();
-        showBtn.text("Show Photo");
-      }
-    });
-    $(".result-container").hide();
-    $("#detail-container").show();
-  };
-
-  const removeCurrentMarks = () => {
-    if (currentMarks.length > 0) {
-      currentMarks.forEach(cMarker => {
-        cMarker.setMap(null);
-      });
+        $(".nav").hide("fast");
+      } // saves the new position for iteration.
+      scrollPos = document.body.getBoundingClientRect().top;
     }
-  };
-
-  //create mark on map
-  const creatMark = (lat, lng) => {
-    let marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, lng),
-      icon: icons["info"].icon,
-      map: map
-    });
-    currentMarks.push(marker);
-    return marker;
-  };
-
-  const measureDist = (lat1, lng1, lat2, lng2) => {
-    let R = 6378.137;
-    let dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
-    let dLng = (lng2 * Math.PI) / 180 - (lng1 * Math.PI) / 180;
-    let a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos((lat1 * Math.PI) / 180) *
-        Math.cos((lat2 * Math.PI) / 180) *
-        Math.sin(dLng / 2) *
-        Math.sin(dLng / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let d = R * c;
-    return d * 1000;
-  };
-
-  const sortByDist = list => {
-    //insertion sort
-    for (let i = 1; i < list.length; i++) {
-      let j = i - 1;
-      while (j > -1 && list[j].dist >= list[j + 1].dist) {
-        let temp = list[j];
-        list[j] = list[j + 1];
-        list[j + 1] = temp;
-        j--;
-      }
-    }
-  };
-
-  const getMapSize = () => {
-    let container = document.getElementById("map");
-    let width = container.offsetWidth;
-    let height = container.offsetHeight;
-    return { w: width, h: height };
-  };
-
-  const loadMoreList = (liArr, step, btn) => {
-    let cNums = liArr.length,
-      pp = (cp = step); //pp=pause_point cp=current_point
-    if (cNums <= step) {
-      pp = cNums;
-      btn.hide();
-    } else {
-      btn.on("click", () => {
-        if (pp <= cNums) {
-          pp += step;
-          while (cp < pp && cp < cNums) {
-            $(createCard(liArr[cp])).insertBefore(btn);
-            cp++;
-          }
-        }
-        if (pp >= cNums) btn.hide(); //operation after loading all items
-      });
-    }
-    //default load
-    for (var i = 0; i < pp; i++) {
-      $(createCard(liArr[i])).insertBefore(btn);
-    }
-  };
+  });
 }
